f730b41fa91f7c6aa097c04f77e8b1f250e7dabd
shaid30044 <kaona30044@gmail.com>
2023-09-02 12:52:44 +0600

    trying sort by view

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// handling tabs
+
 const handleData = async () => {
   const res = await fetch(
     "https://openapi.programming-hero.com/api/videos/categories"
@@ -20,9 +22,9 @@ const handleData = async () => {
 
     tabContainer.appendChild(div);
   });
-
-  console.log(data);
 };
+
+// handling cards
 
 const handleCard = async (categoryId) => {
   const res = await fetch(
@@ -30,39 +32,34 @@ const handleCard = async (categoryId) => {
   );
   const data = await res.json();
 
+  // no content
+
   const noContent = document.getElementById("no-content");
   const dataLength = data.data.length;
 
-  if (dataLength == "0") {
+  if (!dataLength) {
     noContent.removeAttribute("hidden");
   } else {
     noContent.setAttribute("hidden", "");
   }
 
-  // if (dataLength === false) {
-  //   noContent.removeAttribute("hidden");
-  // } else {
-  //   noContent.setAttribute("hidden", "");
-  // }
+  // sort by view
 
-  // const sortByView = document
-  //   .getElementById("sort-by-view")
-  //   .addEventListener("click", () => {
-  //     data.data.forEach((view) => {
-  //       const viewString = view.others.views;
-  //       const viewFloat = parseFloat(viewString);
-  //       const sortView = viewFloat * 1000;
-  //       const viewArray = [sortView];
-  //       const newArray = [];
+  const array = [];
+  const sortByView = () => {
+    data.data.forEach((sort) => {
+      var sortView = parseFloat(sort.others.views);
 
-  //       viewArray.forEach(() => {
-  //         const sortByViewBtn = Math.max(...viewArray);
-  //         newArray.push(sortByViewBtn);
+      array.push(sortView);
+    });
+    const arraySort = array.sort((a, b) => b - a);
 
-  //         console.log(sortByViewBtn, newArray);
-  //       });
-  //     });
-  //   });
+    console.log(arraySort);
+  };
+
+  sortByView();
+
+  // show cards by category
 
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerText = "";
@@ -78,7 +75,7 @@ const handleCard = async (categoryId) => {
     div.innerHTML = `
     <div>
         <div class="relative">
-            <img class="rounded-lg w-full h-44" src=${card?.thumbnail} alt="" />
+            <img class="rounded-lg w-full h-44" src=${card.thumbnail} alt="" />
             <p
               class="absolute bottom-3 right-3 text-[10px] text-white rounded-md px-2 py-1 ${
                 card.others.posted_date ? "" : "hidden"
@@ -89,19 +86,19 @@ const handleCard = async (categoryId) => {
          </div>
          <div class="flex justify-start items-start gap-3 pt-4 md:pt-5">
             <img class="w-10 h-10 rounded-full" src=${
-              card?.authors[0]?.profile_picture
+              card.authors[0].profile_picture
             } alt="" />
             <div>
               <h3 class="font-bold font-color-3">${card?.title}</h3>
               <div class="flex gap-2 py-2">
                 <p class="text-sm font-color-4">${
-                  card?.authors[0]?.profile_name
+                  card.authors[0].profile_name
                 }</p>
                 <img src=${
-                  card?.authors[0]?.verified ? "./image/badge.png" : "-"
+                  card.authors[0].verified ? "./image/badge.png" : "-"
                 } alt="" />
               </div>
-              <p class="text-sm font-color-4">${card?.others?.views} views</p>
+              <p class="text-sm font-color-4">${card.others.views} views</p>
             </div>
          </div>
     </div>
