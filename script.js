4a9fb0984dd82c9eaeab0aa9118a6c49081feea4
shaid30044 <kaona30044@gmail.com>
2023-09-02 22:43:26 +0600

    sort by view

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// handling tabs
-
+// handle tabs
 const handleData = async () => {
   const res = await fetch(
     "https://openapi.programming-hero.com/api/videos/categories"
@@ -12,101 +11,109 @@ const handleData = async () => {
     const div = document.createElement("div");
 
     div.innerHTML = `
-    <button
-    onclick="handleCard('${tab.category_id}')"
+      <button
+        onclick="handleCard('${tab.category_id}')"
         class="btn normal-case md:text-lg font-medium font-color-1 hover:text-white focus:text-white btn-color-2 hover:bg-[#ff1f3d] focus:bg-[#ff1f3d] rounded-md"
-    >
+      >
         ${tab.category}
-    </button>
-  `;
+      </button>
+    `;
 
     tabContainer.appendChild(div);
   });
+
+  handleCard("1000", true);
 };
 
-// handling cards
+// handle cards
+let sortByViewDescending = true;
+let currentCategoryId = "1000";
 
-const handleCard = async (categoryId) => {
-  const res = await fetch(
-    `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
-  );
-  const data = await res.json();
+const handleCard = async (categoryId, sortByView = false) => {
+  if (currentCategoryId !== categoryId || sortByView) {
+    currentCategoryId = categoryId;
 
-  // no content
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
+    );
+    const data = await res.json();
 
-  const noContent = document.getElementById("no-content");
-  const dataLength = data.data.length;
+    // handle no content
+    const noContent = document.getElementById("no-content");
+    const dataLength = data.data.length;
 
-  if (!dataLength) {
-    noContent.removeAttribute("hidden");
-  } else {
-    noContent.setAttribute("hidden", "");
-  }
+    if (!dataLength) {
+      noContent.removeAttribute("hidden");
+    } else {
+      noContent.setAttribute("hidden", "");
+    }
 
-  // sort by view
+    // sort cards by view count
+    const sortCardsByView = () => {
+      data.data.sort(
+        (a, b) => parseFloat(b.others.views) - parseFloat(a.others.views)
+      );
+    };
 
-  const array = [];
-  const sortByView = () => {
-    data.data.forEach((sort) => {
-      var sortView = parseFloat(sort.others.views);
+    // Sort the cards by view count
+    if (sortByView) {
+      sortByViewDescending = !sortByViewDescending;
+      sortCardsByView();
+    }
 
-      array.push(sortView);
-    });
-    const arraySort = array.sort((a, b) => b - a);
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
 
-    console.log(arraySort);
-  };
+    data.data.forEach((card) => {
+      const dateString = card.others.posted_date;
+      const date = parseFloat(dateString);
+      const hrs = Math.floor(date / 3600);
+      const min = Math.floor((date % 3600) / 60);
 
-  sortByView();
+      const div = document.createElement("div");
 
-  // show cards by category
-
-  const cardContainer = document.getElementById("card-container");
-  cardContainer.innerText = "";
-
-  data.data.forEach((card) => {
-    const dateString = card.others.posted_date;
-    const date = parseFloat(dateString);
-    const hrs = Math.floor(date / 3600);
-    const min = Math.floor((date % 3600) / 60);
-
-    const div = document.createElement("div");
-
-    div.innerHTML = `
-    <div>
-        <div class="relative">
-            <img class="rounded-lg w-full h-44" src=${card.thumbnail} alt="" />
-            <p
-              class="absolute bottom-3 right-3 text-[10px] text-white rounded-md px-2 py-1 ${
-                card.others.posted_date ? "" : "hidden"
-              } ${card.others.posted_date ? "bg-[#171717]" : "bg-transparent"}"
-            >
-            ${card.others.posted_date ? `${hrs}hrs ${min}min ago` : ""}
+      div.innerHTML = `
+        <div>
+          <div class="relative">
+            <img class="rounded-lg w-full h-44" src="${
+              card.thumbnail
+            }" alt="" />
+            <p class="absolute bottom-3 right-3 text-[10px] text-white rounded-md px-2 py-1 ${
+              card.others.posted_date ? "" : "hidden"
+            } ${card.others.posted_date ? "bg-[#171717]" : "bg-transparent"}">
+              ${card.others.posted_date ? `${hrs}hrs ${min}min ago` : ""}
             </p>
-         </div>
-         <div class="flex justify-start items-start gap-3 pt-4 md:pt-5">
-            <img class="w-10 h-10 rounded-full" src=${
+          </div>
+          <div class="flex justify-start items-start gap-3 pt-4 md:pt-5">
+            <img class="w-10 h-10 rounded-full" src="${
               card.authors[0].profile_picture
-            } alt="" />
+            }" alt="" />
             <div>
               <h3 class="font-bold font-color-3">${card?.title}</h3>
               <div class="flex gap-2 py-2">
                 <p class="text-sm font-color-4">${
                   card.authors[0].profile_name
                 }</p>
-                <img src=${
+                <img src="${
                   card.authors[0].verified ? "./image/badge.png" : "-"
-                } alt="" />
+                }" alt="" />
               </div>
-              <p class="text-sm font-color-4">${card.others.views} views</p>
+              <p class="text-sm font-color-4">${
+                card.others.views ? card.others.views : arraySort
+              } views</p>
             </div>
-         </div>
-    </div>
-  `;
+          </div>
+        </div>
+      `;
 
-    cardContainer.appendChild(div);
-  });
+      cardContainer.appendChild(div);
+    });
+  }
+};
+
+// handle sorting by view count
+const sortCardsByView = () => {
+  handleCard(currentCategoryId, true);
 };
 
 handleData();
-handleCard("1000");
